8bf1537c3f109095aad4aed0d605338a2c74ea98
Jeff Scott Brown <brownj@objectcomputing.com>
2022-03-29 10:53:31 -0500

    add static initializer

--- grails-app/domain/torbjorn/boost/constants/Role.groovy
+++ grails-app/domain/torbjorn/boost/constants/Role.groovy
@@ -6,4 +6,8 @@ import grails.compiler.GrailsCompileStatic
 class Role {
     public final static long ADMIN = 1
     public final static long USER = 2
+    static {
+        System.out.println(ADMIN);
+        System.out.println(USER);
+    }
 }
--- grails-app/services/torbjorn/boost/constants/RoleService.groovy
+++ grails-app/services/torbjorn/boost/constants/RoleService.groovy
@@ -1,6 +1,7 @@
 package torbjorn.boost.constants
 
 class RoleService {
+
     void doWhatever() {
         System.out.println(Role.ADMIN);
         System.out.println(Role.USER);
